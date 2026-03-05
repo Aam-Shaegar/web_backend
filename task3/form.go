8a544df63579da4c64309541d17c2e1b1831229f
Dmitry Solovev <dimalexandrovich2006@gmail.com>
2026-03-05 13:37:21 +0300

    New CGI output

--- task3/form.go
+++ task3/form.go
@@ -14,13 +14,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	name := r.FormValue("name")
+	phone := r.FormValue("phone")
+	email := r.FormValue("email")
+	birthday := r.FormValue("birthdate")
+	gender := r.FormValue("gender")
+	bio := r.FormValue("bio")
+	contract := r.FormValue("contract")
+	languages := r.Form["languages[]"]
 	fmt.Fprintln(w, "<h1>CGI работает</h1>")
 
-	for key, values := range r.Form {
-		for _, value := range values {
-			fmt.Fprintf(w, "%s = %s <br>", key, value)
-		}
+	fmt.Fprintf(w, `
+	<html>
+	<head>
+	<title>Form result</title>
+	</head>
+	<body>
+
+	<h1>Submitted data</h1>
+
+	Name: %s <br>
+	Phone: %s <br>
+	Email: %s <br>
+	Birthdate: %s <br>
+	Gender: %s <br>
+	Bio: %s <br>
+	Contract accepted: %s <br>
+
+	<h3>Languages:</h3>
+	<ul>
+	`, name, phone, email, birthday, gender, bio, contract)
+
+	for _, lang := range languages {
+		fmt.Fprintf(w, "<li>%s</li>", lang)
 	}
+
+	fmt.Fprintf(w, `
+	</ul>
+
+	</body>
+	</html>
+	`)
+
 }
 
 func main() {
